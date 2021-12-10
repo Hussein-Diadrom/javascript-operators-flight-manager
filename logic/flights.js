e180394e1add08face84b98b82e5769287607bd8
Hussein-Diadrom <95290186+Hussein-Diadrom@users.noreply.github.com>
2021-12-10 13:50:14 +0100

    s

--- logic/flights.js
+++ logic/flights.js
@@ -1,7 +1,12 @@
 function Flights() {
 function calculateNumberOfFlights(NumPassengers,flightCap){
     let n  ;
-
+    if(NumPassengers<0){
+        throw new Error("The number of passengers must be a positive integer value");
+    }
+    if( flightCap<0){
+        throw new Error("The capacity of the flight must be a positive integer value");
+    }
     if(NumPassengers%flightCap===0){
         n = NumPassengers/flightCap;
     }else{
