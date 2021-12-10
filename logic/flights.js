74e98d028723fe0dac8322f6f53198ed628f339c
Hussein-Diadrom <95290186+Hussein-Diadrom@users.noreply.github.com>
2021-12-10 13:28:24 +0100

    Update flights.js

--- logic/flights.js
+++ logic/flights.js
@@ -1,7 +1,14 @@
 function Flights() {
 function calculateNumberOfFlights(NumPassengers,flightCap){
-        return (NumPassengers/flightCap)+(NumPassengers%flightCap);
-}
+    let n  ;
 
+    if(NumPassengers%flightCap===0){
+        n = NumPassengers/flightCap;
+    }else{
+        n=(NumPassengers/flightCap)+1;
+    }
+    return n ; 
+}
+return{calculateNumberOfFlights};
 }
 
